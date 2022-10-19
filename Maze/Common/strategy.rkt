cd568c506b191f0a99945b9c130dd6409de33769
Zach OBrien <zachobrien99@gmail.com>
2022-10-19 17:58:36 -0400

    wip: Strategy scaffolding

--- Maze/Common/strategy.rkt
+++ Maze/Common/strategy.rkt
@@ -11,8 +11,9 @@
 
 (provide
  (contract-out
-  [move?      contract?]
-  [strategy?  contract?]
+  [move?          contract?]
+  [strategy?      contract?]
+  [player-state?  contract?]
   [riemann-strategy   strategy?]
   [euclidean-strategy strategy?]))
      
@@ -37,21 +38,31 @@
 ;; interpretation: A Move has a direction to shift a row or column, the index of the
 ;;                 row or column to shift, the number of degrees to rotate the spare
 ;;                 tile, and a position to move the currently active player to after the shift
-(struct move [shift-direction? natural-number/c orientation? grid-posn?])
+(struct move [shift-direction idx orientation pos])
 
+
+;; A PlayerState is a structure:
+;;    (struct Board Tile Player)
+;; interpretation: A player knows the board, the extra tile, and all of its information
+(struct player-state [board extra-tile player])
+  
 
 ;; A Strategy is a function:
 ;;    (-> Gamestate Move)
 ;; interpretation: A strategy examines a gamestate and determines a move for the currently active
 ;;                 player to make
-(define strategy? (-> gamestate? move?))
+(define strategy? (-> player-state? move?))
 
 
 ;; --------------------------------------------------------------------
 ;; FUNCTIONALITY IMPLEMENTATION
 
 
+;; PlayerState -> Move
+;; Determine the player's move using the riemann strategy
 (define (riemann-strategy state) 0)
 
 
+;; PlayerState -> Move
+;; Determine the player's move using the euclidean strategy
 (define (euclidean-strategy state) 0)
