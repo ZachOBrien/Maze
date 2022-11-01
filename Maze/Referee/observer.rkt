b342043155f0e7fb1187dce1c8b17b0e2582265b
crozzi <rozzi.c@northeastern.edu>
2022-11-01 18:30:43 -0400

    wip: save and button functionality observer

---
+++ Maze/Referee/observer.rkt
@@ -0,0 +1,41 @@
+#lang racket
+
+(require racket/gui)
+
+(define frame (new frame%
+                   [label "Example"]
+                   [width 400]
+                   [height 400]))
+
+(define MSG "STARTING")
+
+(define (draw dc)
+  (send dc set-scale 3 3)
+  (send dc set-text-foreground "blue")
+  (send dc draw-text MSG 0 0))
+
+(define canvas (new canvas% [parent frame]
+                    [paint-callback (lambda (canvas dc) (draw dc))]))
+
+
+; Make a button in the frame
+(new button% [parent frame]
+             [label "Click Me"]
+             ; Callback procedure for a button click:
+             [callback (lambda (button event)
+                         (begin
+                           (set! MSG "button 1 was clicked")
+                           (send canvas refresh-now)))])
+
+; Make a button in the frame
+(new button% [parent frame]
+             [label "Save File"]
+             ; Callback procedure for a button click:
+             [callback (lambda (button event)
+                         (let
+                             ([file-name (put-file)])
+                           (with-output-to-file file-name (λ () (write MSG)))))])
+
+                              
+
+(send frame show #t)
